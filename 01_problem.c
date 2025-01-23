e4c968d038a226ba81824907d989e7fb920f65e5
Arpan-collab <deyr00081@gmail.com>
2025-01-23 07:34:10 +0600

    make it beautiful

--- 01_problem.c
+++ 01_problem.c
@@ -8,6 +8,7 @@ int main()
     printf("give the number\n");
     scanf("%d", &number);
 
+    printf("%d multiplication table is\n", number);
     for (int i = 0; i < 10; i++)
     {
         int multiplication = (number * (i + 1));
